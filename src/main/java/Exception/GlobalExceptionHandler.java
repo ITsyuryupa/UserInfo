8a307449d94dec9721801652ad85d8734e8a3597
ITsyuryupa <openingthedoor@yandex.ru>
2024-11-18 19:59:48 +0400

    add exceptionHandler

---
+++ src/main/java/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package Exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleException(ResponseStatusException ex) {
+        return new ResponseEntity<>(ex.getMessage(), ex.getStatusCode());
+    }
+}
+
